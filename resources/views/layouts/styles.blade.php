ed0e6039d1155bd43abe7ec168d6756ecb5d2c35
João Mendes <62259008+cenamendes@users.noreply.github.com>
2024-03-14 15:33:21 +0000

    [upd] logo

--- resources/views/layouts/styles.blade.php
+++ resources/views/layouts/styles.blade.php
@@ -1,4 +1,4 @@
 <link rel="stylesheet" href="assets/css/siqtheme.css">
 
-<link rel="apple-touch-icon" sizes="76x76" href="{{asset('logo/sanipower.png')}}">
-<link rel="icon" type="image/png" sizes="96x96" href="{{asset('logo/sanipower.png')}}">
+<link rel="apple-touch-icon" sizes="76x76" href="{{asset('logo/icon.png')}}">
+<link rel="icon" type="image/png" sizes="96x96" href="{{asset('logo/icon.png')}}">
