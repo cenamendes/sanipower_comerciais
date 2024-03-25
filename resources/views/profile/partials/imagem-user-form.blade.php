f698b1319d6189948537b9fe1022b7771c92c4ae
João Mendes <62259008+cenamendes@users.noreply.github.com>
2024-03-25 18:32:09 +0000

    [upd] imagem de perfil

--- resources/views/profile/partials/imagem-user-form.blade.php
+++ resources/views/profile/partials/imagem-user-form.blade.php
@@ -1,5 +1,6 @@
 <section>
     <header class="imagem-user">
+        {{-- @livewire('profile.update-profile-imagem') --}}
         @livewire('profile.update-profile-imagem')
     </header>
 </section>
