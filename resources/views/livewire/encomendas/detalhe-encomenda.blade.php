42f00804bf13e8df5f25d41f96983cc32b6cd30b
ViniciusKarv <131013621+Vinicuskarv@users.noreply.github.com>
2024-05-17 09:08:22 +0100

    add

--- resources/views/livewire/encomendas/detalhe-encomenda.blade.php
+++ resources/views/livewire/encomendas/detalhe-encomenda.blade.php
@@ -41,7 +41,7 @@
                 </li>
             </ul>
         </div>
-        <div class="card-body" id="scrollModalBody" style="overflow-y:auto;max-height:64vh;">
+        <div class="card-body" id="scrollModalBody" style="overflow-y:auto;max-height:64vh;padding-right: 0;">
             <div class="tab-content">
 
                 <div class="tab-pane fade {{ $tabDetail }}" id="tab4">
@@ -387,7 +387,7 @@
 
                                     <div class="row justify-content-between">
                                         <div class="col">
-                                            <div class="input-group" id="checkboxSidbar">
+                                            <div class="input-group d-block d-md-none" id="checkboxSidbar">
                                                 <input id="checkbox" type="checkbox">
                                                 <label class="toggle" for="checkbox">
                                                     <div id="bar1" class="bars"></div>
@@ -437,9 +437,9 @@
                                             <br>
                                         </div>
                                     </div>
-                                    <div class="row">
+                                    <div class="row" style="justify-content: flex-end;">
 
-                                        <div class="navbar2 col-3">
+                                        <div class="navbar2 col-3 d-none d-md-block">
                                             @php
                                                 $contaCat = 0;
                                             @endphp
@@ -448,12 +448,12 @@
                                                     $contaCat++;
                                                 @endphp
                                                 @if (!empty($category->family))
-                                                    <button class="accordion2">{{ $category->id }} -
+                                                    <button class="accordion2" style="background: #5f77921c;">{{ $category->id }} -
                                                         {{ $category->name }}<span
                                                             class="arrow">&#9660;</span></button>
                                                     <div class="panel2">
                                                         @foreach ($category->family as $family)
-                                                            <button class="accordion2">{{ $family->id }} -
+                                                            <button class="accordion2" style="background-color: #1791ba26;">{{ $family->id }} -
                                                                 {{ $family->name }}<span
                                                                     class="arrow">&#9660;</span></button>
                                                             <div class="panel2">
@@ -469,7 +469,7 @@
                                             @endforeach
                                         </div>
 
-                                        <div class="row col-9">
+                                        <div class="row col-md-9">
 
                                             <div wire:loading wire:target="searchProduct">
                                                 <div id="filtroLoader" style="display: block;">
