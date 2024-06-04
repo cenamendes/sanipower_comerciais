608d78852c24cfa141d3192c03dab0a510088e57
ViniciusKarv <131013621+Vinicuskarv@users.noreply.github.com>
2024-06-04 18:31:16 +0100

    add

--- resources/views/livewire/encomendas/detalhe-encomenda.blade.php
+++ resources/views/livewire/encomendas/detalhe-encomenda.blade.php
@@ -1062,7 +1062,7 @@
                                         </tr>
                                      @empty
                                         <tr>
-                                            <td colspan="5" class="text-center">No items in the cart.</td>
+                                            <td colspan="5" class="text-center">Nenhum produto no carrinho</td>
                                         </tr>
                                     @endforelse
                                 </tbody>
