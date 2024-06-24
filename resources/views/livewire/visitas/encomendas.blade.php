d3f464ac1aca8c4eeaa76c23f5f87a37b9cf804b
LucasEpifanio <lucasepifaniodev@gmail.com>
2024-06-24 15:26:36 +0100

    commit-lucas

--- resources/views/livewire/visitas/encomendas.blade.php
+++ resources/views/livewire/visitas/encomendas.blade.php
@@ -1,7 +1,7 @@
 <div>
 
     <style>
-         @media (max-width: 1100px) {
+        @media (max-width: 1100px) {
             .btn:not(:disabled):not(.disabled) {
                 cursor: pointer;
                 font-size: 0.9rem;
@@ -10,6 +10,7 @@
                 margin-top: 0.6rem;
             }
         }
+
         @media (max-width: 680px) {
             .btn:not(:disabled):not(.disabled) {
                 cursor: pointer;
@@ -74,16 +75,15 @@
                 </div>
                 <div class="card-body">
 
-                    <div id="dataTables_wrapper" class="dataTables_wrapper container"
-                        style="margin-left:0px;padding-left:0px;margin-bottom:10px;">
+                    <div id="dataTables_wrapper" class="dataTables_wrapper container" style="margin-left:0px;padding-left:0px;margin-bottom:10px;">
                         <div class="left">
                             <label>
                                 Mostrar
                                 <select name="perPage" wire:model="perPage">
-                                    <option value="10" @if ($perPage == 10) selected @endif>10</option>
-                                    <option value="25" @if ($perPage == 25) selected @endif>25</option>
-                                    <option value="50" @if ($perPage == 50) selected @endif>50</option>
-                                    <option value="100" @if ($perPage == 100) selected @endif>100
+                                    <option value="10" @if ($perPage==10) selected @endif>10</option>
+                                    <option value="25" @if ($perPage==25) selected @endif>25</option>
+                                    <option value="50" @if ($perPage==50) selected @endif>50</option>
+                                    <option value="100" @if ($perPage==100) selected @endif>100
                                     </option>
                                 </select>
                                 registos
@@ -100,31 +100,34 @@
                                     <th>Total</th>
                                     <th>Estado</th>
                                     <th>Ações</th>
+                                    <th>Detalhe Encomenda</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 @foreach ($detalhesEncomenda as $detalhe)
-                                    <tr>
-                                        <td>{{ date('Y-m-d', strtotime($detalhe->date)) }}</td>
-                                        <td>{{ $detalhe->order }}</td>
-                                        <td>{{ $detalhe->total }}</td>
-                                        <td>{{ $detalhe->status }}</td>
-                                        <td><button type="button"
-                                                class="btn btn-primary"wire:click="comentarioModal({{ json_encode($detalhe->id) }}, {{ json_encode($detalhe->order) }})"><i
-                                                    class="ti ti-plus"></i> Comentário</button>
-                                            @php
-                                                $cmt = \App\Models\Comentarios::where('stamp', $detalhe->id)
-                                                    ->where('tipo', 'encomendas')
-                                                    ->get();
-                                            @endphp
-                                            @if ($cmt->count() > 0)
-                                                <button type="button" class="btn btn-primary"
-                                                    wire:click="verComentario({{ json_encode($detalhe->id) }})">
-                                                    Ver Comentário
-                                                </button>
-                                            @endif
-                                        </td>
-                                    </tr>
+                                <tr>
+                                    <td>{{ date('Y-m-d', strtotime($detalhe->date)) }}</td>
+                                    <td>{{ $detalhe->order }}</td>
+                                    <td>{{ $detalhe->total }}</td>
+                                    <td>{{ $detalhe->status }}</td>
+                                    <td><button type="button" class="btn btn-primary" wire:click="comentarioModal({{ json_encode($detalhe->id) }}, {{ json_encode($detalhe->order) }})"><i class="ti ti-plus"></i> Comentário</button>
+                                        @php
+                                        $cmt = \App\Models\Comentarios::where('stamp', $detalhe->id)
+                                        ->where('tipo', 'encomendas')
+                                        ->get();
+                                        @endphp
+                                        @if ($cmt->count() > 0)
+                                        <button type="button" class="btn btn-primary" wire:click="verComentario({{ json_encode($detalhe->id) }})">
+                                            Ver Comentário
+                                        </button>
+                                        @endif
+                                    </td>
+                                    <td>
+                                        <button type="button" class="btn btn-primary" wire:click="detalheEncomendaModal({{ json_encode($detalhe->id) }})">
+                                            <i class="ti ti-plus"></i> Ver Encomenda
+                                        </button>
+                                    </td>
+                                </tr>
                                 @endforeach
                             </tbody>
                         </table>
@@ -141,8 +144,7 @@
 
     <!-- MODALS -->
 
-    <div class="modal fade" id="modalComentario" tabindex="-1" role="dialog" aria-labelledby="modalComentario"
-        aria-hidden="true">
+    <div class="modal fade" id="modalComentario" tabindex="-1" role="dialog" aria-labelledby="modalComentario" aria-hidden="true">
         <div class="modal-dialog modal-xl modal-dialog-centered" role="document">
             <div class="modal-content">
                 <div class="modal-header">
@@ -156,23 +158,20 @@
                         <div class="card-body">
                             <label>Comentário</label>
                             <div class="input-group">
-                                <textarea type="text" class="form-control" cols="4" rows="6" style="resize: none;"
-                                    wire:model.defer="comentarioEncomenda"></textarea>
+                                <textarea type="text" class="form-control" cols="4" rows="6" style="resize: none;" wire:model.defer="comentarioEncomenda"></textarea>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-outline-dark" data-dismiss="modal">Fechar</button>
-                    <button type="button" class="btn btn-outline-primary"
-                        wire:click="sendComentario({{ json_encode($encomendaID) }})">Adicionar</button>
+                    <button type="button" class="btn btn-outline-primary" wire:click="sendComentario({{ json_encode($encomendaID) }})">Adicionar</button>
                 </div>
             </div>
         </div>
     </div>
 
-    <div class="modal fade" id="modalVerComentario" tabindex="-1" role="dialog"
-        aria-labelledby="modalVerComentarioLabel" aria-hidden="true">
+    <div class="modal fade" id="modalVerComentario" tabindex="-1" role="dialog" aria-labelledby="modalVerComentarioLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
             <div class="modal-content">
                 <div class="modal-header">
@@ -184,12 +183,11 @@
                 <div class="modal-body">
                     <div class="timeline-wrapper">
                         @isset($comentario)
-                            @foreach ($comentario as $comentarios)
-                                <div class="timeline-item"
-                                    data-date="{{ $comentarios->created_at }} &#8594; {{ $comentarios->user->name }}">
-                                    <p>{{ $comentarios->comentario }}</p>
-                                </div>
-                            @endforeach
+                        @foreach ($comentario as $comentarios)
+                        <div class="timeline-item" data-date="{{ $comentarios->created_at }} &#8594; {{ $comentarios->user->name }}">
+                            <p>{{ $comentarios->comentario }}</p>
+                        </div>
+                        @endforeach
                         @endisset
                     </div>
                 </div>
@@ -200,23 +198,76 @@
         </div>
     </div>
 
-    <!----->
-    <script>
-        document.addEventListener('livewire:load', function() {
-            Livewire.hook('message.sent', () => {
-                document.getElementById('loader').style.display = 'block';
-            });
 
-            // Oculta o loader quando o Livewire terminar de carregar
-            Livewire.hook('message.processed', () => {
-                document.getElementById('loader').style.display = 'none';
-            });
+
+
+    <div class="modal fade" id="detalheEncomendaModal" tabindex="-1" role="dialog" aria-labelledby="detalheEncomendaModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-xl modal-dialog-centered" style="margin: 1.75rem auto;" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="detalheEncomendaModalLabel">Detalhes da Encomenda</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Referencia</th>
+                                <th>Descrição</th>
+                                <th>Quantidade</th>
+                                <th>Preço</th>
+                                <th>Desconto</th>
+                                <th>Desconto 2</th>
+                                <th>Total</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($detalhesEncomenda[0]->lines as $line)
+                            <tr>
+                                <td>{{ $line->reference }}</td>
+                                <td>{{ $line->description }}</td>
+                                <td style="text-align:center">{{ $line->quantity }}</td>
+                                <td style="text-align:center">{{ $line->price }}</td>
+                                <td style="text-align:center">{{ $line->discount }}</td>
+                                <td style="text-align:center">{{ $line->discount2 }}</td>
+                                <td style="text-align:center">{{ $line->total }}</td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal" style="cursor:pointer">Fechar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+</div>
+
+
+
+<!----->
+<script>
+    document.addEventListener('livewire:load', function() {
+        Livewire.hook('message.sent', () => {
+            document.getElementById('loader').style.display = 'block';
         });
 
-
-        document.addEventListener('abrirModalVerComentario', function() {
-            $('#modalVerComentario').modal('show');
+        // Oculta o loader quando o Livewire terminar de carregar
+        Livewire.hook('message.processed', () => {
+            document.getElementById('loader').style.display = 'none';
         });
-    </script>
+    });
+
+
+    document.addEventListener('abrirModalVerComentario', function() {
+        $('#modalVerComentario').modal('show');
+    });
+
+    document.addEventListener('openDetalheEncomendaModal', function() {
+        $('#detalheEncomendaModal').modal('show');
+    });
+</script>
 
 </div>
