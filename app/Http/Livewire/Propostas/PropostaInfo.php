fc11ec6534754e997eee16f8a4ecb827045df9ce
ViniciusKarv <131013621+Vinicuskarv@users.noreply.github.com>
2024-08-07 17:01:51 +0100

    add

--- app/Http/Livewire/Propostas/PropostaInfo.php
+++ app/Http/Livewire/Propostas/PropostaInfo.php
@@ -239,8 +239,6 @@ class PropostaInfo extends Component
 
     public function adjudicarProposta($proposta)
     {
-        dd($this->selectedItemsAdjudicar);       
-       
         $flag = false;
 
         foreach($this->selectedItemsAdjudicar as $item)
